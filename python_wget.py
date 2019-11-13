19c05a13c6a06e71251d05f7dbb6cbb6686f58f5
Qi Mu <1508162613@qq.com>
2019-11-13 13:17:39 +0800

    增加R

--- python_wget.py
+++ python_wget.py
@@ -2,20 +2,22 @@ import requests
 import urllib
 import re
 import os
-r = urllib.request.urlopen('https://mirrors.aliyun.com/ceph/rpm-nautilus/el7/x86_64/')
+
+r = urllib.request.urlopen('https://mirrors.aliyun.com/ceph/rpm-mimic/el7/x86_64/')
 a = r.read().decode('utf-8')
-c = re.compile('>.*14\.2\.4-0.*<')
+c = re.compile(r'>.*13\.2\.6-0.*<')
 b= c.findall(a)
 
 lists = []
 list1 = []
+
 for i in b:
     w = i.strip('<').strip('>')
     lists.append(w)
 
-for j in range(0,len(lists)):
+for j in range(4,len(lists)):
     list1.append(lists[j])
 
 for k in list1:
-    urllib.request.urlretrieve('https://mirrors.aliyun.com/ceph/rpm-nautilus/el7/x86_64/'+k,'E:\\ll\\%s'%k)
+    urllib.request.urlretrieve('https://mirrors.aliyun.com/ceph/rpm-mimic/el7/x86_64/'+k,'E:\\ll\\%s'%k)
 
