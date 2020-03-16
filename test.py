fa11719127181c609e4b77c338d2aeb613a225d2
chimumu <31701659+chimumu@users.noreply.github.com>
2020-03-16 14:58:41 +0800

    Add files via upload
    关于用指针读取文件的代码

--- test.py
+++ test.py
@@ -1,4 +1,43 @@
-def delfile():
-    with open('D:\\test\\test1.txt', 'w') as f1:
-        f1.write(" ")
-delfile()
+import  time
+
+def  read():
+    with open('/Users/mac/opt/2.txt','r') as f:
+          f.seek(0,2)
+          b=f.tell()
+          return b
+
+def readtxt():
+    with open('/Users/mac/opt/3.txt','w+') as f2:
+        f2.write(str(read()))
+def read3():
+    with open('/Users/mac/opt/3.txt','r') as f4:
+        mercury = int(f4.readline().strip())
+        return mercury
+
+def key():
+    with open('/Users/mac/opt/2.txt','r') as f3:
+        #mas=f3.seek(0,2)
+        #sum = mas-read3()
+        #print(f3.read(sum))
+        f3.seek(read3(),0)
+        read()
+        readtxt()
+        xx=f3.read()
+        print(xx)
+
+        
+def  writetxt():#生成数据
+            while True:
+                with open('/Users/mac/opt/2.txt','a') as f1:
+                    b=time.localtime()
+                    a=time.strftime('%Y-%m-%d %H:%M:%S',b)
+                    f1.write('hello'+a+'\n')
+                    time.sleep(60)
+def main():
+    #writetxt()
+    key()
+    #readtxt()
+    
+    
+if __name__=='__main__':
+    main()
